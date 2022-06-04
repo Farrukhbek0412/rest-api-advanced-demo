37430cfe945732248c9172f2e3d58bc6fff130eb
Farrux <Farrux@gmail.com>
2022-06-04 19:31:14 +0500

    fixed bugs

--- persistence/src/main/java/com/epam/esm/repository/order/OrderQueries.java
+++ persistence/src/main/java/com/epam/esm/repository/order/OrderQueries.java
@@ -1,7 +1,6 @@
 package com.epam.esm.repository.order;
 
 public interface OrderQueries {
-
     String GET_ALL_ORDERS = "select o from OrderEntity o";
     String GET_ORDER_BY_USER_ID = "select o from OrderEntity o where o.user.id = :id";
     String GET_ORDER_BY_USER_ID_AND_ORDER_ID
